bd420dd3ffe5f4e33c7f4386e4927916b4e07222
Evgueni Antonov <strayferalmusic@gmail.com>
2026-02-05 19:23:54 +0200

    Fixed: asciiquarium was not present in trixie repos

--- scripts/50inst-games.sh
+++ scripts/50inst-games.sh
@@ -11,6 +11,5 @@ sudo apt -y install pacman4console
 sudo apt -y install nudoku
 sudo apt -y install bastet  # Tetris clone
 sudo apt -y install frotz
-sudo apt -y install asciiquarium
 sudo apt -y install cowsay
 sudo apt -y install vitetris
