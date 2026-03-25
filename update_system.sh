7907a4100a07af46bf33661aea5d76246b4d7276
Evgueni Antonov <strayferalmusic@gmail.com>
2026-03-25 15:13:26 +0200

    Added script to easily update the entire system.

---
+++ update_system.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+export DEBIAN_FRONTEND="noninteractive"
+trap 'echo "ERROR in ${BASH_SOURCE[0]} at line ${LINENO}: $BASH_COMMAND"; exit 130' INT
+
+echo ""
+echo "UPDATING THE SYSTEM..."
+echo ""
+
+echo "Acquiring updates..."
+sudo apt update
+sudo apt upgrade
+
+echo "Downloading additional updates..."
+git -C wiki-tui
+git -C mdterm
+git -C lazytail
+
+echo "Installing additional updates..."
+cargo install --path wiki-tui/
+cargo install --path mdterm/
+cargo install rgx-cli
+cargo install xleak
+cargo install chess-tui
+
+bash lazytail/install.sh
+lazytail init
+
+echo ""
+echo "SYSTEM UPDATED."
